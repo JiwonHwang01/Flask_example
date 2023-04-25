36ae73ce7a357a961f180ac5fe82c2d6df582d61
JiwonHwang01 <cutty82432@gmail.com>
2023-04-25 10:22:05 +0900

    [feat] Add CRUD method

--- server.py
+++ server.py
@@ -1,8 +1,7 @@
-from flask import Flask
-import random
+from flask import Flask, request, redirect
 
 app = Flask(__name__)
-
+nextId = 7
 parts = [
     {'id': 1, 'title':'Back', 'body' : 'Back day'},
     {'id': 2, 'title':'Chest', 'body' : 'Chest day'},
@@ -12,7 +11,13 @@ parts = [
     {'id': 6, 'title':'Abs', 'body' : 'Plus Abs'}
 ]
 
-def templates(order, contents):
+def templates(order, contents, id=None):
+    contextUI = ''
+    if id != None:
+        contextUI = f'''
+            <li><a href="/update/{id}/">update</a></li>
+            <li><form action="/delete/{id}/" method ="POST"><input type ="submit" value="delete"></form></li>
+        '''
     return f'''<!doctype html>
     <html>
         <body>
@@ -21,6 +26,10 @@ def templates(order, contents):
                     {order}
                 </ol>
                 {contents}
+                <ul>
+                    <li><a href="/create/">create</a></li>
+                    {contextUI}
+                </ul>
         </body>
     </html>    
     '''
@@ -51,9 +60,67 @@ def read(id):
             body = part['body']
             break
     
-    return templates(getOrder(), f'<h2>{title}</h2>{body}')
+    return templates(getOrder(), f'<h2>{title}</h2>{body}',id)
 
-@app.route('/create/')
+@app.route('/create/', methods=['GET', 'POST'])
 def create():
-    pass
+    if request.method == 'GET':
+
+        content = '''
+            <form action="/create/" method="POST">
+                <p><input type="text" name="title" placeholder="title"></p>
+                <p><textarea name="body" placeholder="body"></textarea></p>
+                <p><input type="submit" value="create"></p>
+            </form>
+        '''
+        return templates(getOrder(), content)
+    elif request.method == 'POST':
+        global nextId
+        title = request.form['title']
+        body = request.form['body']
+        newTopic = {'id': nextId, 'title': title, 'body': body}
+        parts.append(newTopic)
+        url = '/read/'+ str(nextId) + '/'
+        nextId += 1
+        return redirect(url)
+    
+@app.route('/update/<int:id>/', methods=['GET', 'POST'])
+def update(id):
+    if request.method == 'GET':
+        title = ''
+        body = ''
+        for part in parts:
+            if id == part['id']:
+                title = part['title']
+                body = part['body']
+                break
+        content = f'''
+            <form action="/update/{id}/" method="POST">
+                <p><input type="text" name="title" placeholder="title" value="{title}"></p>
+                <p><textarea name="body" placeholder="body">{body}</textarea></p>
+                <p><input type="submit" value="update"></p>
+            </form>
+        '''
+        return templates(getOrder(), content)
+    elif request.method == 'POST':
+        global nextId
+        title = request.form['title']
+        body = request.form['body']
+
+        for part in parts:
+            if id == part['id']:
+                part['title'] = title
+                part['body'] = body
+                break
+        url = '/read/'+ str(id) + '/' 
+        return redirect(url)
+    
+@app.route('/delete/<int:id>/', methods=['POST'])
+def delete(id):
+    for part in parts:
+        if id == part['id']:
+            parts.remove(part)
+            break
+    return redirect('/')
+
 app.run(debug=True)
